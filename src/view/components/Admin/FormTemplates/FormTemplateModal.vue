d5f8e8d8d5be5bc07e40b88e79bf64ebb2719ba8
Matin-Laptop <matink1490@gmail.com>
2025-01-25 00:58:32 +0330

    Fix bugs.

--- src/view/components/Admin/FormTemplates/FormTemplateModal.vue
+++ src/view/components/Admin/FormTemplates/FormTemplateModal.vue
@@ -1,54 +1,54 @@
 <template>
-  <base-modal
-      :title="!data? 'تعریف فرم جدید': 'ویرایش فرم'"
-      :visible="visible"
-      @close="$emit('update:visible', false)"
-      @submit="submit">
-    <div class="row">
-      <div class="col-12">
-        <base-text-field
-            label="عنوان فرم"
-            v-model="model.title"
-        />
-      </div>
+    <base-modal
+            :title="!data? 'تعریف فرم جدید': 'ویرایش فرم'"
+            :visible="visible"
+            @close="$emit('update:visible', false)"
+            @submit="submit">
+        <div class="row">
+            <div class="col-12">
+                <base-text-field
+                        label="عنوان فرم"
+                        v-model="model.title"
+                />
+            </div>
 
 
-    </div>
-  </base-modal>
+        </div>
+    </base-modal>
 </template>
 
 <script>
 
 export default {
-  name: "FormTemplateModal",
-  emits: ['update', 'add'],
-  props: {
-    visible: Boolean,
-    data: Object,
-  },
-  data() {
-    return {
-      model: {
-        title: null,
-      }
+    name: "FormTemplateModal",
+    emits: ['update', 'add'],
+    props: {
+        visible: Boolean,
+        data: Object,
+    },
+    data() {
+        return {
+            model: {
+                title: null,
+            }
+        }
+    },
+    methods: {
+        submit() {
+            let payload = {
+                title: this.model.title,
+            }
+            if (this.data) {
+                this.httpPut(`/form-template/${this.data.id}`, payload, result => {
+                    this.$emit('update', result);
+                })
+            } else {
+                this.httpPost(`/form-template/`, payload, result => {
+                    this.$emit('add', result);
+                })
+            }
+        }
     }
-  },
-  methods: {
-    submit() {
-      let payload = {
-        title: this.model.title,
-      }
-      if (this.data) {
-        this.httpPut(`/form-template/${this.data.id}`, payload, result => {
-          this.$emit('update', result);
-        })
-      } else {
-        this.httpPost(`/form-template`, payload, result => {
-          this.$emit('add', result);
-        })
-      }
-    }
-  }
 }
 </script>
 
